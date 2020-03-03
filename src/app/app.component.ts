94e1e7ee1a56c48b8ff852af663c807ef58be157
Hareesh <59041457+HareeshSeetha@users.noreply.github.com>
2020-03-03 21:10:27 +0530

    contactus

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
+import { Component } from "@angular/core";
+import { FormControl, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'GmartApp';
+  router;
 
+  gotocontactus() {
+    this.router.navigate(["/contactus"]);
+  }
 }
