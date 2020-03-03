87b33baeaebf7487521b8520593e6deaf9efef18
Amarender Tangella <ATangella@morneaushepell.com>
2020-03-02 19:37:44 -0500

    material forms test

--- src/app/header-component/header-component.component.ts
+++ src/app/header-component/header-component.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms'
 
+let obj = {
+  text1: "value1",
+  text2: "value2",
+  text3: "value3",
+  text4: "value4",
+}
 @Component({
   selector: 'app-header-component',
   templateUrl: './header-component.component.html',
   styleUrls: ['./header-component.component.scss']
 })
 export class HeaderComponentComponent implements OnInit {
-
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  email= new FormControl('', [Validators.required
+  ,Validators.email]);
+
 
 }
