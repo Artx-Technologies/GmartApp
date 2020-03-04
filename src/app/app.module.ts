2c74551da4aee5dab4d05442038b6a0f5b06198d
Pavankumarreddy-0 <60994234+Pavankumarreddy-0@users.noreply.github.com>
2020-03-04 11:20:36 +0530

    getting changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,6 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
-<<<<<<< HEAD
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-
-import { HomeComponentComponent } from './home-component/home-component.component';
-import { ProductsComponentComponent } from './products-component/products-component.component';
-import { ContactusComponentComponent } from './contactus-component/contactus-component.component';
-import { LandingImageComponentComponent } from './landing-image-component/landing-image-component.component';
-import { ContentSectionComponentComponent } from './content-section-component/content-section-component.component';
-import { HeaderComponentComponent } from './header-component/header-component.component';
-import { FooterComponentComponent } from './footer-component/footer-component.component';
-=======
 import { AppRoutingModule, routingcomponents } from "./app-routing.module";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
@@ -56,7 +44,6 @@ import { MatTableModule } from "@angular/material/table";
 import { MatSortModule } from "@angular/material/sort";
 import { MatPaginatorModule } from "@angular/material/paginator";
 import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
->>>>>>> master
 
 @NgModule({
   declarations: [
