f852ef8e8ce14bb056ab69ea8037b0bd6f39734c
Hareesh <59041457+HareeshSeetha@users.noreply.github.com>
2020-03-05 06:51:38 +0530

    contact us

--- src/app/contactus-component/contactus-component.component.ts
+++ src/app/contactus-component/contactus-component.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormGroup } from "@angular/forms";
 
 @Component({
-  selector: 'app-contactus-component',
-  templateUrl: './contactus-component.component.html',
-  styleUrls: ['./contactus-component.component.scss']
+  selector: "app-contactus-component",
+  templateUrl: "./contactus-component.component.html",
+  styleUrls: ["./contactus-component.component.scss"]
 })
 export class ContactusComponentComponent implements OnInit {
+  formGroup: FormGroup;
+  titleAlert: string = "This Filed Is Reqired";
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit() {}
 
+  getErrorEmail() {}
 }
