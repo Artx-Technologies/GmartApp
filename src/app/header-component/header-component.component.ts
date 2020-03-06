869ebeb805fa66217a65c864a8f65062bf72c006
Hareesh <59041457+HareeshSeetha@users.noreply.github.com>
2020-03-06 12:16:49 +0530

    c

--- src/app/header-component/header-component.component.ts
+++ src/app/header-component/header-component.component.ts
@@ -1,68 +1,48 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
-import { Observable } from 'rxjs';
+import { Component, OnInit } from "@angular/core";
+import { faCoffee, faBars } from "@fortawesome/free-solid-svg-icons";
+import { faGoodreadsG } from "@fortawesome/free-brands-svg-icons";
+import { Router } from "@angular/router";
 
 let obj = {
   text1: "value1",
   text2: "value2",
   text3: "value3",
-  text4: "value4",
-}
+  text4: "value4"
+};
 @Component({
-  selector: 'app-header-component',
-  templateUrl: './header-component.component.html',
-  styleUrls: ['./header-component.component.scss']
+  selector: "app-header-component",
+  templateUrl: "./header-component.component.html",
+  styleUrls: ["./header-component.component.scss"]
 })
 export class HeaderComponentComponent implements OnInit {
-  formGroup: FormGroup;
-  titleAlert: string = 'This field is required';
-  post: any = '';
-  ex= [];
+  faCoffee = faCoffee;
+  currentPath: string;
+  faBars = faBars;
+  faIc = faGoodreadsG;
+  HomeClassH: boolean = false;
+  ProductClassH: boolean = false;
+  ContactClassH: boolean = false;
+  constructor(private router: Router) {}
+  ngOnInit() {}
 
-  constructor(private formBuilder: FormBuilder) { }
-
-  ngOnInit() {
-    this.createForm();
+  goHome() {
+    this.router.navigate(["/home"]);
+    this.HomeClassH = true;
+    this.ProductClassH = false;
+    this.ContactClassH = false;
+    // console.log(hero);
   }
 
-  createForm() {
-    let gmailRegex: RegExp = /^[a-z0-9](\.?[a-z0-9]){5,}@g(oogle)?mail\.com$/
-    let emailregex: RegExp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-    this.formGroup = this.formBuilder.group({
-      'email': [null, [Validators.required, Validators.pattern(emailregex)]],
-      'name': [null, Validators.required],
-      'password': [null, [Validators.required, this.checkPassword]],
-      'description': [null, [Validators.required, Validators.minLength(5), Validators.maxLength(10)]],
-      'validate': ''
-    });
+  goProducts() {
+    this.router.navigate(["/products"]);
+    this.HomeClassH = false;
+    this.ProductClassH = true;
+    this.ContactClassH = false;
   }
-
-  get name() {
-    return this.formGroup.get('name') as FormControl
+  goContacts() {
+    this.router.navigate(["/contactus"]);
+    this.HomeClassH = false;
+    this.ProductClassH = false;
+    this.ContactClassH = true;
   }
-
-  checkPassword(control) {
-    let enteredPassword = control.value
-    let passwordCheck = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})/;
-    return (!passwordCheck.test(enteredPassword) && enteredPassword) ? { 'requirements': true } : null;
-  }
-
-  getErrorEmail() {
-    return this.formGroup.get('email').hasError('required') ? 'Field is required' :
-      this.formGroup.get('email').hasError('pattern') ? 'Not a valid emailaddress' :
-        this.formGroup.get('email').hasError('alreadyInUse') ? 'This emailaddress is already in use' : '';
-  }
-
-  getErrorPassword() {
-    return this.formGroup.get('password').hasError('required') ? 'Field is required (at least eight characters, one uppercase letter and one number)' :
-      this.formGroup.get('password').hasError('requirements') ? 'Password needs to be at least eight characters, one uppercase letter and one number' : '';
-  }
-
-  onSubmit() {
-    // this.post = post;
-    console.log(this.formGroup.value)
-    this.ex.push(this.formGroup.value);
-  }
-
-
 }
