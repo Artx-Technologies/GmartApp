0bd0085b1907a16f0964de5fb779c3d651c1e81d
Hareesh <59041457+HareeshSeetha@users.noreply.github.com>
2020-03-06 12:21:23 +0530

    c

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { ContactusComponentComponent } from "./contactus-component/contactus-component.component";
+import { HomeComponentComponent } from "./home-component/home-component.component";
+import { ProductsComponentComponent } from "./products-component/products-component.component";
 
 const routes: Routes = [
-  {
-    path: "contactus",
-    component: ContactusComponentComponent
-  }
+  { path: "home", component: HomeComponentComponent },
+  { path: "products", component: ProductsComponentComponent },
+  { path: "contactus", component: ContactusComponentComponent }
 ];
 
 @NgModule({
