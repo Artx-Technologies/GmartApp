7607e3621ddb418de2bb608911e40ec17be71db7
Hareesh <59041457+HareeshSeetha@users.noreply.github.com>
2020-03-06 12:13:45 +0530

    c

--- src/app/footer-component/footer-component.component.ts
+++ src/app/footer-component/footer-component.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import {
+  faFacebookF,
+  faTwitter,
+  faPinterestP
+} from "@fortawesome/free-brands-svg-icons";
+import {
+  faMapMarkerAlt,
+  faPhone,
+  faEnvelope
+} from "@fortawesome/free-solid-svg-icons";
 
 @Component({
-  selector: 'app-footer-component',
-  templateUrl: './footer-component.component.html',
-  styleUrls: ['./footer-component.component.scss']
+  selector: "app-footer-component",
+  templateUrl: "./footer-component.component.html",
+  styleUrls: ["./footer-component.component.scss"]
 })
 export class FooterComponentComponent implements OnInit {
+  fb = faFacebookF;
+  tw = faTwitter;
+  faPhone = faPhone;
+  faMapMarkerAlt = faMapMarkerAlt;
+  faPinterestP = faPinterestP;
+  faEnvelope = faEnvelope;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
