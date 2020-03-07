8f79297d4f5fd2b554af785c70fcb5364ab3ad91
chinna <chinnabhai944@gmail.com>
2020-03-07 14:56:24 +0530

    increased thickness to the hr tag in header

--- src/app/products-component/products-component.component.ts
+++ src/app/products-component/products-component.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit,Renderer2, ElementRef, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { HeaderComponentComponent } from '../header-component/header-component.component';
 
 @Component({
   selector: 'app-products-component',
@@ -8,15 +7,9 @@ import { HeaderComponentComponent } from '../header-component/header-component.c
   styleUrls: ['./products-component.component.scss']
 })
 export class ProductsComponentComponent implements OnInit {
-  constructor(private router: Router,private renderer: Renderer2,private elementRef:ElementRef) { }
- // @ViewChild("addHr", {static: false}) private addHrvar: ElementRef;
- @ViewChild(HeaderComponentComponent,{static: true}) private haderelement: HeaderComponentComponent;
+  constructor(private router: Router) { }
   ngOnInit(): void {
-    if(this.router.url === "/products"){
-      console.log("YEss");
-      //this.renderer.appendChild(this.d1.nativeElement,hr);
-      console.log(this.haderelement);
-    }
+  
   }
 
 }
