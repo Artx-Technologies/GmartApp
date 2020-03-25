67f688cfab942b62593c59bea344af548ff8c770
chinna <chinnabhai944@gmail.com>
2020-03-25 19:12:27 +0530

    added dashboard tabs

--- src/app/renter-dashboard/renter-dashboard.component.ts
+++ src/app/renter-dashboard/renter-dashboard.component.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-
+import { faCaretRight } from "@fortawesome/free-solid-svg-icons";
 @Component({
   selector: 'app-renter-dashboard',
   templateUrl: './renter-dashboard.component.html',
   styleUrls: ['./renter-dashboard.component.scss']
 })
 export class RenterDashboardComponent implements OnInit {
-
+  rightArrowFilled =faCaretRight;
   constructor() { }
-
+  whichoneclicked:String = "Dashboard";
   ngOnInit() {
   }
 
